842f2507b3f66e2d35e24bd50c21e623a2458e42
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2025-09-16 08:56:46 -0400

    namespace imports

--- src/blush/combiner.rs
+++ src/blush/combiner.rs
@@ -1,16 +1,9 @@
-use crate::{
-    parser::ast::{Expression, ExpressionKind, Program, UseTailItem},
-    span::Span,
-};
+use crate::parser::ast::{Expression, ExpressionKind, FunctionArgument, Program, UseTailItem};
 use std::error::Error;
 
 pub struct Combiner<'a> {
     files: Vec<(&'a str, &'a Program)>,
 }
-
-// TODO:
-// - namespace imports
-// - naming conflicts (might have to mangle imported variable names)
 
 impl<'a> Combiner<'a> {
     pub const fn new(programs: Vec<(&'a str, &'a Program)>) -> Self {
@@ -29,12 +22,13 @@ impl<'a> Combiner<'a> {
         self.resolve_imports(main_file.1)
     }
 
+    #[allow(clippy::too_many_lines)]
     fn resolve_imports(&self, main_file: &Program) -> Result<Program, Box<dyn Error>> {
         let mut new_main_file = Program(vec![]);
 
         for expr in &main_file.0 {
             match &expr.kind {
-                crate::parser::ast::ExpressionKind::Use { segments, tail } => {
+                ExpressionKind::Use { segments, tail } => {
                     let mut ref_program_name = segments.join("/");
                     ref_program_name.push_str(".lv");
                     let Some((_, ref_program)) = self
@@ -48,7 +42,84 @@ impl<'a> Combiner<'a> {
                     };
 
                     if tail.is_empty() {
-                        todo!("namespaced imports")
+                        let mut variables_in_main_file = vec![];
+                        for expr in &main_file.0 {
+                            variables_in_main_file.extend(Self::referenced_variables(expr, vec![]));
+                        }
+                        let Some(namespace_name) = segments.last() else {
+                            return Err("Invalid use statement (no segments)".into());
+                        };
+                        let variables_to_import = variables_in_main_file
+                            .iter()
+                            .filter_map(|(name, namespace)| {
+                                if namespace.as_ref() == Some(namespace_name) {
+                                    Some(name)
+                                } else {
+                                    None
+                                }
+                            })
+                            .collect::<Vec<_>>();
+
+                        let mut definitions_from_ref_program = ref_program
+                            .0
+                            .iter()
+                            .filter_map(|expr| match &expr.kind {
+                                ExpressionKind::VariableDecl {
+                                    name,
+                                    value,
+                                    mutable,
+                                    ty,
+                                } => {
+                                    if variables_to_import.contains(&name) {
+                                        Some(Expression::new(
+                                            ExpressionKind::VariableDecl {
+                                                // mangle import name
+                                                name: format!("{namespace_name}_{name}"),
+                                                value: value.clone(),
+                                                mutable: *mutable,
+                                                ty: ty.clone(),
+                                            },
+                                            expr.span,
+                                        ))
+                                    } else {
+                                        None
+                                    }
+                                }
+                                _ => None,
+                            })
+                            .collect::<Vec<_>>();
+
+                        let vars_referenced_in_ref_program_defs = definitions_from_ref_program
+                            .iter()
+                            .flat_map(|expr| Self::referenced_variables(expr, vec![]))
+                            .collect::<Vec<_>>();
+
+                        let mut uses_used_in_ref_program_defs = ref_program
+                            .0
+                            .iter()
+                            .filter_map(|expr| match &expr.kind {
+                                ExpressionKind::Use { segments, tail } => {
+                                    if vars_referenced_in_ref_program_defs.iter().any(
+                                        |(name, namespace)| {
+                                            Self::matches_import(
+                                                name,
+                                                namespace.as_ref(),
+                                                segments,
+                                                tail,
+                                            )
+                                        },
+                                    ) {
+                                        Some(expr.clone())
+                                    } else {
+                                        None
+                                    }
+                                }
+                                _ => None,
+                            })
+                            .collect::<Vec<_>>();
+
+                        new_main_file.0.append(&mut uses_used_in_ref_program_defs);
+                        new_main_file.0.append(&mut definitions_from_ref_program);
                     } else {
                         for tail_item in tail {
                             let Some(corresponding_def) =
@@ -73,28 +144,20 @@ impl<'a> Combiner<'a> {
                                 .iter()
                                 .filter_map(|expr| match &expr.kind {
                                     ExpressionKind::Use { segments, tail } => {
-                                        Some((segments, tail))
+                                        if referenced_vars.iter().any(|(name, namespace)| {
+                                            Self::matches_import(
+                                                name,
+                                                namespace.as_ref(),
+                                                segments,
+                                                tail,
+                                            )
+                                        }) {
+                                            Some(expr.clone())
+                                        } else {
+                                            None
+                                        }
                                     }
                                     _ => None,
-                                })
-                                .filter(|(segments, tail_items)| {
-                                    referenced_vars.iter().any(|(name, namespace)| {
-                                        Self::matches_import(
-                                            name,
-                                            namespace.as_ref(),
-                                            segments,
-                                            tail_items,
-                                        )
-                                    })
-                                })
-                                .map(|(segments, tail_items)| {
-                                    Expression::new(
-                                        ExpressionKind::Use {
-                                            segments: segments.clone(),
-                                            tail: tail_items.clone(),
-                                        },
-                                        Span::from_range(0, 0), // TODO: real span
-                                    )
                                 })
                                 .collect::<Vec<_>>();
 
@@ -103,19 +166,8 @@ impl<'a> Combiner<'a> {
                         }
                     }
                 }
-
-                ExpressionKind::BoolLiteral(_)
-                | ExpressionKind::IntLiteral(_)
-                | ExpressionKind::Ident { .. }
-                | ExpressionKind::Block(_)
-                | ExpressionKind::Prefix { .. }
-                | ExpressionKind::Infix { .. }
-                | ExpressionKind::VariableDecl { .. }
-                | ExpressionKind::Function { .. }
-                | ExpressionKind::FunctionCall { .. } => {
-                    // TODO: some of these will need to be handled differently, like namespaced
-                    // variable idents
-                    new_main_file.0.push(expr.clone());
+                _ => {
+                    new_main_file.0.push(Self::mangle_var_names(expr));
                 }
             }
         }
@@ -131,12 +183,104 @@ impl<'a> Combiner<'a> {
         }
     }
 
+    fn mangle_var_names(expr: &Expression) -> Expression {
+        match &expr.kind {
+            ExpressionKind::Ident { name, namespace } => namespace.as_ref().map_or_else(
+                || expr.clone(),
+                |namespace| {
+                    Expression::new(
+                        ExpressionKind::Ident {
+                            name: format!("{namespace}_{name}"),
+                            namespace: None,
+                        },
+                        expr.span,
+                    )
+                },
+            ),
+            ExpressionKind::Block(expressions) => Expression::new(
+                ExpressionKind::Block(expressions.iter().map(Self::mangle_var_names).collect()),
+                expr.span,
+            ),
+            ExpressionKind::Prefix {
+                operator,
+                expression,
+            } => Expression::new(
+                ExpressionKind::Prefix {
+                    operator: *operator,
+                    expression: Box::new(Self::mangle_var_names(expression)),
+                },
+                expr.span,
+            ),
+            ExpressionKind::Infix {
+                left,
+                operator,
+                right,
+            } => Expression::new(
+                ExpressionKind::Infix {
+                    left: Box::new(Self::mangle_var_names(left)),
+                    operator: *operator,
+                    right: Box::new(Self::mangle_var_names(right)),
+                },
+                expr.span,
+            ),
+            ExpressionKind::VariableDecl {
+                name,
+                value,
+                mutable,
+                ty,
+            } => Expression::new(
+                ExpressionKind::VariableDecl {
+                    name: name.clone(),
+                    value: Box::new(Self::mangle_var_names(value)),
+                    mutable: *mutable,
+                    ty: ty.clone(),
+                },
+                expr.span,
+            ),
+            ExpressionKind::Function {
+                parameters,
+                return_type,
+                body,
+            } => Expression::new(
+                ExpressionKind::Function {
+                    parameters: parameters.clone(),
+                    return_type: return_type.clone(),
+                    body: Box::new(Self::mangle_var_names(body)),
+                },
+                expr.span,
+            ),
+            ExpressionKind::FunctionCall {
+                name,
+                namespace,
+                arguments,
+            } => Expression::new(
+                ExpressionKind::FunctionCall {
+                    name: name.clone(),
+                    namespace: namespace.clone(),
+                    arguments: arguments
+                        .iter()
+                        .map(|arg| FunctionArgument {
+                            label: arg.label.clone(),
+                            value: Self::mangle_var_names(&arg.value),
+                        })
+                        .collect(),
+                },
+                expr.span,
+            ),
+            ExpressionKind::BoolLiteral(_)
+            | ExpressionKind::IntLiteral(_)
+            | ExpressionKind::Use { .. } => expr.clone(),
+        }
+    }
+
     fn referenced_variables(
         expr: &Expression,
         base: Vec<(String, Option<String>)>,
     ) -> Vec<(String, Option<String>)> {
         match &expr.kind {
-            ExpressionKind::BoolLiteral(_) | ExpressionKind::IntLiteral(_) => base,
+            ExpressionKind::BoolLiteral(_)
+            | ExpressionKind::IntLiteral(_)
+            | ExpressionKind::Use { .. } => base,
             ExpressionKind::Ident { name, namespace } => {
                 let mut new_base = base;
                 new_base.push((name.clone(), namespace.clone()));
@@ -166,7 +310,6 @@ impl<'a> Combiner<'a> {
                     Self::referenced_variables(&arg.value, acc)
                 })
             }
-            ExpressionKind::Use { .. } => todo!("not sure what to do here yet"),
         }
     }
 
